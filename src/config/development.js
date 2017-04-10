31f79986acffb133d5d8157bb26927c2af384098
marioharper <mario@marioharper.me>
2017-04-10 09:25:42 -0500

    cookie stuff

--- src/config/development.js
+++ src/config/development.js
@@ -1,5 +1,5 @@
 const AUTH0_CALLBACK_URL = 'http://localhost:5000';
 const PET_WHISPERER_API_BASE = "http://localhost:8080";
-const COOKIE_DOMAIN = ".localhost:5000"; 
+const COOKIE_DOMAIN = "localhost"; 
 
 export { AUTH0_CALLBACK_URL, PET_WHISPERER_API_BASE, COOKIE_DOMAIN }
